0a4ed3a1369d8fae0505a61e8d8daa960a10c040
Alexey Grachov <grachov.alexey@gmail.com>
2015-06-12 13:43:11 +0300

    Add benchmark.

--- golligator_test.go
+++ golligator_test.go
@@ -1,6 +1,19 @@
 package golligator
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
+
+var events chan *Event
+var g *Golligator
+
+func TestMain(m *testing.M) {
+	config := &Config{Port: 8081, ServerUrl: "http://localhost:8081"}
+	g = NewGolligator(config)
+	events = g.Listen()
+	os.Exit(m.Run())
+}
 
 func TestNewGolligator(t *testing.T) {
 	config := &Config{Port: 8081, ServerUrl: "http://localhost:8081"}
@@ -17,13 +30,19 @@ func TestNewGolligator(t *testing.T) {
 }
 
 func TestListenAndSend(t *testing.T) {
-	config := &Config{Port: 8081, ServerUrl: "http://localhost:8081"}
-	g := NewGolligator(config)
-	events := g.Listen()
 	message := createAddFrameworkEvent("1", "user", "framework")
 	g.Send(&Event{Type: "AddFramework", Message: message})
 	event := <-events
 	if event.Message.(*AddFramework).GetFrameworkId().GetValue() != "1" {
 		t.Fail()
+	}
+}
+
+func BenchmarkSendAndReceive(b *testing.B) {
+	message := createAddFrameworkEvent("1", "user", "framework")
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		g.Send(&Event{Type: "AddFramework", Message: message})
+		<-events
 	}
 }
